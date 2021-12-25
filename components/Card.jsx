008e4e5c7d3bd8832cf1e3a47fab9c5ed0c5f8a9
mircea_007 <mirceareb@gmail.com>
2021-12-25 12:23:27 +0200

    add simple card class

---
+++ components/Card.jsx
@@ -0,0 +1,23 @@
+export default function Card( props ) {
+  let hasTitle    = !!props.title;
+  let hasChildren = !!props.children;
+  let hasImg      = !!props.imgUrl;
+  let hasSubTitle = !!props.subTitle;
+  
+  if( !hasChildren )
+    throw 'Card: content missing';
+
+  if( !hasTitle )
+    throw 'Card: title missing';
+  
+  return (
+    <div className="rounded-md shadow-md bg-white flex flex-row overflow-hidden">
+      <div class="flex flex-col gap-2 p-4">
+        <h1 className="text-center text-2xl uppercase font-semibold">{ props.title }</h1>
+        <hr className="border-gray-200"/>
+        { hasSubTitle ? <h2 className="uppercase text-xl font-bold text-blue-500">{ props.subTitle }</h2> : null}
+        { props.children }
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
+import Card from '../components/Card.jsx'
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="main font-sans">
+      <h1 className="w-full text-center p-8 text-blue-400 text-6xl font-bold">
+        YT questions
+      </h1>
+      <div className="flex flex-row gap-4 p-3">
+        <Card title="abcd">
+          Officia in tempor qui deserunt ex labore proident velit proident cupidatat quis sed id fugiat veniam in ea do est qui eiusmod nisi esse sint sunt voluptate cupidatat duis magna magna voluptate mollit consectetur et.
+        </Card>
+        <Card title="abcd" subTitle="abcd">
+          Officia in tempor qui deserunt ex labore proident velit proident cupidatat quis sed id fugiat veniam in ea do est qui eiusmod nisi esse sint sunt voluptate cupidatat duis magna magna voluptate mollit consectetur et.
+        </Card>
+      </div>
+    </div>
   )
 }
